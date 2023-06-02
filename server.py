49baf616c8c4ad3621f6156797e7591b5495d4c9
jorted-runner <jorted.runner1996@gmail.com>
2023-06-02 12:29:28 -0500

    update port

--- server.py
+++ server.py
@@ -202,5 +202,5 @@ def delete_post(index):
     return redirect(url_for('user_dests')) 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT"))
     app.run(host="0.0.0.0", port=port)
