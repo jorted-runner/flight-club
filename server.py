daa8f1089e792f523a00f07eed5ebf6a8bbc62ef
jorted-runner <jorted.runner1996@gmail.com>
2023-06-02 16:10:23 -0500

    Update Time of Scheduled Job

--- server.py
+++ server.py
@@ -206,7 +206,7 @@ def delete_post(index):
     return redirect(url_for('user_dests')) 
 
 if __name__ == "__main__":
-    schedular.add_job(id = 'Scheduled task', func= send_daily_alerts, trigger = 'cron', month= '*', day= '*', hour = '16', minute = '5')
+    schedular.add_job(id = 'Scheduled task', func= send_daily_alerts, trigger = 'cron', month= '*', day= '*', hour = '4')
     schedular.start()
-    port = int(os.environ.get("PORT"))
+    port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", port=port)
