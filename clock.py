b36a3648e691976f1357f5f0e2a5c2ca2686c032
jorted-runner <jorted.runner1996@gmail.com>
2023-06-02 22:03:21 -0500

    Add Clock and Update Procfile

---
+++ clock.py
@@ -0,0 +1,8 @@
+from server import send_daily_alerts
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', day_of_week='*', hour=3, minute=5)
+def scheduled_alert():
+    send_daily_alerts()
