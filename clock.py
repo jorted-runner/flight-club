a3a551ef0357e6df34f4d217b30ab8169623f7c7
jorted-runner <jorted.runner1996@gmail.com>
2023-06-02 22:04:28 -0500

    Update Clock.py

--- clock.py
+++ clock.py
@@ -3,6 +3,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='*', hour=3, minute=5)
+@sched.scheduled_job('cron', day_of_week='*', hour=3, minute=6)
 def scheduled_alert():
     send_daily_alerts()
+
+sched.start()
